7edb1b987f8fed5270d1a53dd4eb8f657c5ce4fd
aligorbor <aligorbor@list.ru>
2021-06-22 21:01:39 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/android2/nasapicture/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.geekbrains.android2.nasapicture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.geekbrains.android2.nasapicture.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ru/geekbrains/android2/nasapicture/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.geekbrains.android2.nasapicture.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
